2ad60b51bc5f0ec4ec46ceeb65eaac99c88a4fc7
CondeArmand <104652384+CondeArmand@users.noreply.github.com>
2023-02-28 23:26:44 -0300

    Projeto Sacola Books

---
+++ Projetos Java/Atividades/IFPI - Programação Orientada a Objetos/Sacola Books/Sacola-Books/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.View.LoginForm;
+
+
+public class Main {
+    public static void main(String[] args) {
+        LoginForm loginForm = new LoginForm(null);
+        loginForm.setVisible(true);
+    }
+}
+
